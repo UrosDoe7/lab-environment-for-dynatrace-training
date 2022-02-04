cb84c5b7cab9eb2c474a434f62395ab976e8529b
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2022-02-04 18:19:36 +0100

    Delete env.sh

--- clean_env/env.sh
+++
@@ -1,6 +0,0 @@
-export NEW_CLI=1
-export MyTenant=yyyy.jzq02463.live.dynatrace.com
-export MyToken=dt0c01.abcdefghij.abcdefghijklmn
-export PaasToken=dt0c01.abcdefghij.abcdefghijklmn
-export list_user="user1@user1.com user2@user2.com"
-export EnableSynthetic=true
