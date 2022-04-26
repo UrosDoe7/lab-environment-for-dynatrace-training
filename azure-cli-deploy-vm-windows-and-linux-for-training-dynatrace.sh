3d312b94b4b0d53f888cea26e13514407fa73600
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2022-04-26 22:26:16 +0200

    Update azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh

--- azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh
+++ azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh
@@ -330,7 +330,7 @@ do
         ###install shellinabox to go to the linux env from a browser (port 443)
         az vm run-command invoke -g "$RESOURCE_GROUP" -n "$DOMAIN" --command-id RunShellScript --scripts "apt-get install shellinabox && sed -i 's/4200/443/g' /etc/default/shellinabox";
 	###install genkey
-	az vm run-command invoke -g "$RESOURCE_GROUP" -n "$DOMAIN" --command-id RunShellScript --scripts "cd /home/user"$X$i" && echo TEST && pwd && echo "$RSAPUB" >> .ssh/authorized_keys";
+	az vm run-command invoke -g "$RESOURCE_GROUP" -n "$DOMAIN" --command-id RunShellScript --scripts "cd /home/user"$X$i"/.ssh && ls && echo TEST && pwd && echo "$RSAPUB" >> authorized_keys && echo TOTO >> authorized_keys";
 	
 	###Install EasyTravel
         if [[ $EASYTRAVEL_ENV = [Y] ]]
