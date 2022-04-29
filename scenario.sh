bea5c3ee59f5df2952f9b0349e8bac1f5ef9b3e3
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2022-04-29 13:10:36 +0200

    Update scenario.sh

--- scenario.sh
+++ scenario.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-sh start-stop-easytravel.sh start
-
-sleep 2m
-
-sh start-stop-easytravel.sh issue
-
-sleep 5m
-
+echo "step 1 start"
 sh start-stop-easytravel.sh restart
 
+echo "sleep 2m"
+sleep 2m
+
+echo "sleep issue"
+sh start-stop-easytravel.sh issue
+
+echo "sleep 5m"
 sleep 5m
 
+echo "sleep restart"
+sh start-stop-easytravel.sh restart
+
+echo "sleep 5m"
+sleep 5m
+
+echo "sleep restartmongo"
 sh start-stop-easytravel.sh restartmongo
 
+echo "sleep 5m"
 sleep 5m
 
+echo "sleep restart"
 sh start-stop-easytravel.sh restart
