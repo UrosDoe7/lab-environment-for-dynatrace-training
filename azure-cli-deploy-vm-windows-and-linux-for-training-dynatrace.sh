82e51fdf41a0700e151060472d24a231a8ad43e7
Jean-Louis Lormeau <40337213+JLLormeau@users.noreply.github.com>
2022-10-12 11:24:51 +0200

    Update azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh

--- azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh
+++ azure-cli-deploy-vm-windows-and-linux-for-training-dynatrace.sh
@@ -357,7 +357,7 @@ do
 			if [[ $FULL_INSTALLATION = [Y] ]]
                         then
 				export MyTenant=$MyTenant
-				export PaasToken=$PaasToken
+				export PaasToken=$MyToken
                                 az vm run-command invoke -g "$RESOURCE_GROUP" -n "$DOMAIN" --command-id RunShellScript --scripts "cd /home && wget  -O Dynatrace-OneAgent-Linux-latest.sh \"https://"$MyTenant"/api/v1/deployment/installer/agent/unix/default/latest?arch=x86&flavor=default\" --header=\"Authorization: Api-Token "$PaasToken"\" && sudo /bin/sh Dynatrace-OneAgent-Linux-latest.sh --set-host-group=easytravel"$X$i" --set-host-property=env=sandbox";
                         fi				
 			if [[ $FULL_INSTALLATION = [Y] ]]
